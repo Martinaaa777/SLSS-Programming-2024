1bd4abe58c4c62bcc43f7bb27b3560e677bf7f33
martina <martinapanosetti07@gmail.com>
2024-04-11 11:02:23 -0700

    finished mid-unit activity

---
+++ unit-mid2-activity.py
@@ -0,0 +1,15 @@
+# martina panosetti
+# 8 april 2024
+# personal work on unit 1 and 2
+
+# create a function with a parameter, a return, a loop
+
+# counts from 1 to "n" 
+# returns the last counted number
+
+def counter (n):
+    for i in range (1, n +1):
+         print (i)
+    return n
+last_number= counter (100)
+print ("the last couunnted number is:", last_number)
