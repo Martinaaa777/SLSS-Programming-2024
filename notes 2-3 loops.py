ab3e40f54049bb1adc274cdfcce1488aa257dede
martina <martinapanosetti07@gmail.com>
2024-05-16 14:34:42 -0700

    pygame-exercise-jewelthief.py

--- notes 2-3 loops.py
+++ notes 2-3 loops.py
@@ -1,4 +1,48 @@
-#loops
-#martina panosetti
+# loops
+# martina panosetti
 # 5 april 2024
 
+# Print "something" 10 times
+for _ in range(10):
+    print("something")
+
+# create a grocery list
+# do something for each item in the list
+grocery_list = [
+    "blberry muffin",
+    "potato chips",
+    "foil",
+    "orange juice",
+    "rtx 4070 super",
+    "cereal"
+]
+
+# for every item:
+#      * {grocery item}
+#      * {grocery item}
+#      * {grocery item}
+for item in grocery_list:
+    # skip the rest of the list 
+    # if we get to rtx 4070 super
+    if item.lower () == "rtx 4070 super" :
+        print ("i cannot buy it, it is expensive")
+        break # stop looping
+    print (f"*{item}")
+
+# can you count using a for loop ?
+# use a for loop to count to 100
+for i in range (100):
+    print (i+ 1)
+
+
+#while loops are useful for input validation
+# ask the user if they like ice cream
+# if they don't answer yes or no repeat the question
+
+user_answer = input ("do you like ice cream?")
+
+while user_answer not in ["yes", "no", "yeah", "nah"]
+user_answer= input ("seriously, do you like ice cream?")
+
+if user_answer in ["yes" "yeah"]:
+    print("nice , i love ice cream too")
--- pygame-exercise-jewelthief.py
+++ pygame-exercise-jewelthief.py
@@ -13,12 +13,12 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 GRAY = (128, 128, 128)
-
 WIDTH = 1280  # Pixels
 HEIGHT = 720
 SCREEN_SIZE = (WIDTH, HEIGHT)
 
-NUM_COINS = 10
+NUM_COINS = 100
+NUM_ENEMIES = 5
 
 
 class Player(pg.sprite.Sprite):
@@ -27,26 +27,75 @@ class Player(pg.sprite.Sprite):
         super().__init__()
 
         self.image = pg.image.load("./Images/mario.webp")
+        self.images = [
+            pg.image.load("./Images/mario.webp"),
+            pg.transform.flip(pg.image.load("./Images/mario.webp"), True, False),
+        ]
+
+        self.image = self.images[0]
 
         self.rect = self.image.get_rect()
+
+        self.facing = 0  # 0 is right, 1 is left
 
     def update(self):
         """Updates the location of sprite to the mouse cursor"""
         self.rect.centerx = pg.mouse.get_pos()[0]
         self.rect.centery = pg.mouse.get_pos()[1]
+        next_pos = pg.mouse.get_pos()
+
+        if self.rect.centerx > next_pos[0]:
+            self.facing = 1
+        elif self.rect.centerx < next_pos[0]:
+            self.facing = 0
+
+        self.rect.center = next_pos
+        self.image = self.images[self.facing]
 
 
 class Coin(pg.sprite.Sprite):
     def __init__(self):
         super().__init__()
-
         self.image = pg.image.load("./Images/coin.png")
-
         self.rect = self.image.get_rect()
-
         # Randomize initial location
         self.rect.x = random.randrange(0, WIDTH - self.rect.width)
         self.rect.y = random.randrange(0, HEIGHT - self.rect.height)
+
+
+class Goomba(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+
+        self.image = pg.image.load("./Images/goomba.png")
+        self.rect = self.image.get_rect()
+
+        # Spawn in a random location
+        self.rect.x = random.randrange(0, WIDTH - self.rect.width)
+        self.rect.y = random.randrange(0, HEIGHT - self.rect.height)
+
+        # Set initial velocity to some random value
+        self.vel_x = random.choice((-6, -5, -4, 4, 5, 6))
+        self.vel_y = random.choice((-6, -5, -4, 4, 5, 6))
+
+    def update(self):
+        """Move the goomba and bounce it off the edge of the window"""
+        self.rect.x += self.vel_x
+        self.rect.y += self.vel_y
+
+        # Bounce
+        if self.rect.left < 0:
+            self.rect.left = 0  # keep it inside the screen
+            self.vel_x = -self.vel_x
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+            self.vel_x = -self.vel_x
+        if self.rect.top < 0:
+            self.rect.top = 0
+            self.vel_y = -self.vel_y
+        if self.rect.bottom > HEIGHT:
+            self.rect.bottom = HEIGHT
+            self.vel_y = -self.vel_y
 
 
 def start():
@@ -56,63 +105,38 @@ def start():
     pg.mouse.set_visible(False)
 
     # --Game State Variables--
+    # --Game St
+    # ate Variables--
     screen = pg.display.set_mode(SCREEN_SIZE)
     done = False
     clock = pg.time.Clock()
 
+    score = 0
+
+    # -- Sprite Groups
     # All sprites go in this sprite Group
     all_sprites = pg.sprite.Group()
 
     # Coin sprites
     coin_sprites = pg.sprite.Group()
+    enemy_sprites = pg.sprite.Group()
 
+    # Create coins
     for _ in range(NUM_COINS):
         coin = Coin()
 
         all_sprites.add(coin)
         coin_sprites.add(coin)
-
     # Create a player and store it in a variable
     player = Player()
 
     all_sprites.add(player)
 
+    for _ in range(NUM_ENEMIES):
+        enemy = Goomba()
+        all_sprites.add(enemy)
+        enemy_sprites.add(enemy)
+
     pg.display.set_caption("Jewel Thief Clone (Don't sue us Nintendo)")
 
     # --Main Loop--
-    while not done:
-        # --- Event Listener
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                done = True
-
-        # --- Update the world state
-        all_sprites.update()
-
-        # Collision between player and coin_sprites
-        # Get a list of ALL coin_sprites that collide
-        #     with the player
-        # For every coin that collides, print "COLLISION!"
-        coins_collided = pg.sprite.spritecollide(player, coin_sprites, False)
-
-        for coin in coins_collided:
-            print(f"COLLISION at {coin.rect.x}, {coin.rect.y}")
-
-        # --- Draw items
-        screen.fill(WHITE)
-
-        all_sprites.draw(screen)
-
-        # Update the screen with anything new
-        pg.display.flip()
-
-        # --- Tick the Clock
-        clock.tick(60)  # 60 fps
-
-
-def main():
-    start()
-
-
-if __name__ == "__main__":
-    main()
